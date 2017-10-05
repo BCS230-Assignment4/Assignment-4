119fe7f9d61bff116b58e681c4b7abbe35253bf7
RyanLivingston <32498412+RyanLivingston@users.noreply.github.com>
2017-10-05 11:17:08 -0400

    Continuation of last commit

--- Assignment 4/Surgery.cpp
+++ Assignment 4/Surgery.cpp
@@ -7,30 +7,30 @@ using namespace std;
 
 void Surgery::Hand(PatientAccount& patient)
 {
-    cout << "Patient underwent hand surgery!" << endl << "$43,643 has been added to your cost!" << endl;
+    cout << "Patient underwent hand surgery!" << endl << "$43,643 has been added to their cost!" << endl;
     patient.updateSurgery(majorHand);
     
 }
 void Surgery::Spinal(PatientAccount& patient)
 {
-    cout << "Patient underwent spinal surgery!" << endl << "$86,055 has been added to your cost!" << endl;
+    cout << "Patient underwent spinal surgery!" << endl << "$86,055 has been added to their cost!" << endl;
     patient.updateSurgery(majorSpinal);
 }
 void Surgery::Brain(PatientAccount& patient)
 {
-    cout << "Patient underwent brain surgery!" << endl << "$153,537 has been added to your cost!" << endl;
+    cout << "Patient underwent brain surgery!" << endl << "$153,537 has been added to their cost!" << endl;
     patient.updateSurgery(majorBrain);
 
 }
 void Surgery::Cardiac(PatientAccount& patient)
 {
-    cout << "Patient underwent cardiac surgery!" << endl << "$72,834 has been added to your cost!" << endl;
+    cout << "Patient underwent cardiac surgery!" << endl << "$72,834 has been added to their cost!" << endl;
     patient.updateSurgery(majorCardiac);
 
 }
 void Surgery::Knee(PatientAccount& patient)
 {
-    cout << "Patient underwent knee surgery!" << endl << "$43,643 has been added to your cost!" << endl;
+    cout << "Patient underwent knee surgery!" << endl << "$43,643 has been added to their cost!" << endl;
     patient.updateSurgery(majorKnee);
 
 }
