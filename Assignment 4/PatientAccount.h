f5b46ee7b5d54a51c27a192f2f8a90727cd9c27a
RyanLivingston <32498412+RyanLivingston@users.noreply.github.com>
2017-10-05 09:36:27 -0400

    Add PatientAccount header file

---
+++ Assignment 4/PatientAccount.h
@@ -0,0 +1,32 @@
+
+////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                    //
+//  Patient Fees, Patient Acount Class                                                //
+//  Header File                                                                       //
+//  Author: Frederick Aranda                                                          //
+//  Date: October 4, 2017                                                             //
+//  Purpose: To keep track of the total charges of the patient and the daily rate.    //
+//                                                                                    //
+////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef PatientAccount_h
+#define PatientAccount_h
+
+#include <iostream>
+using namespace std;
+
+class PatientAccount{
+    
+private:
+    float totCostPerDay;
+    float dailyRate = 2152;
+    int days;
+    float totalCost;
+    
+public:
+    PatientAccount(float totCostPerDay, int days, float totalCost);
+    float getTotCostPerDay();
+    float getTotalCost(float& totCostPerDay, float totCostOfSurgery, float& totCostOfPharmacy);
+};
+
+#endif /* PatientAccount_h */
