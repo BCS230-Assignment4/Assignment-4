d64320e335fd5a590ba971d7a03dc1e6d6a4fa2e
RyanLivingston <32498412+RyanLivingston@users.noreply.github.com>
2017-10-05 08:17:38 -0400

    Initial Commit

---
+++ Assignment 4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment 4
+//
+//  Created by Ryan Livingston on 10/5/17.
+//  Copyright © 2017 Ryan Livingston. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
