17789911ad021cc39b9fc19efa59404e3ad85c13
RyanLivingston <32498412+RyanLivingston@users.noreply.github.com>
2017-10-05 11:33:25 -0400

    Pharmacy cout format

--- Assignment 4/Pharmacy.cpp
+++ Assignment 4/Pharmacy.cpp
@@ -7,30 +7,30 @@ using namespace std;
 
 void Pharmacy::levothyroxine(PatientAccount& patient)
 {
-    cout << "Patient underwent hand surgery!" << endl << "$43,643 has been added to their cost!" << endl;
+    cout << "Patient purchased levothyroxine!" << endl << "$89 has been added to their cost!" << endl;
     patient.updateSurgery(levothyroxineCost);
     
 }
 void Pharmacy::rosuvastatin(PatientAccount& patient)
 {
-    cout << "Patient underwent spinal surgery!" << endl << "$86,055 has been added to their cost!" << endl;
+    cout << "Patient purchased rosuvastatin!" << endl << "$243 has been added to their cost!" << endl;
     patient.updateSurgery(rosuvastatinCost);
 }
 void Pharmacy::albuterol(PatientAccount& patient)
 {
-    cout << "Patient underwent brain surgery!" << endl << "$153,537 has been added to their cost!" << endl;
+    cout << "Patient purchased albuterol!" << endl << "$543 has been added to their cost!" << endl;
     patient.updateSurgery(albuterolCost);
     
 }
 void Pharmacy::esomeprazole(PatientAccount& patient)
 {
-    cout << "Patient underwent cardiac surgery!" << endl << "$72,834 has been added to their cost!" << endl;
+    cout << "Patient purchased esomeprazole!" << endl << "$133 has been added to their cost!" << endl;
     patient.updateSurgery(esomeprazoleCost);
     
 }
 void Pharmacy::fluticasone(PatientAccount& patient)
 {
-    cout << "Patient underwent knee surgery!" << endl << "$43,643 has been added to their cost!" << endl;
+    cout << "Patient purchased fluticasone!" << endl << "$745 has been added to their cost!" << endl;
     patient.updateSurgery(fluticasoneCost);
     
 }
