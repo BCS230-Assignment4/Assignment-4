4bf0e7a6c9abe06561534b0120edec6117ee2ddc
fredyarnanda <aranfh@farmingdale.edu>
2017-10-05 10:20:59 -0400

    Update PatientAccount.h

--- Assignment 4/PatientAccount.h
+++ Assignment 4/PatientAccount.h
@@ -20,7 +20,7 @@ class PatientAccount{
 private:
     int days;
     float stayCost;
-    float dailyRate = 2152;
+    float dailyRate = 2152; //The average cost per inpatient day in New York for a state/local hospital
     float charges;
     
 public:
